d2a5e9a3c51fb38e1faa74810a046fb6692ad7b8
maximilianknuerr <39460393+maximilianknuerr@users.noreply.github.com>
2023-04-13 12:59:04 +0200

    implemented plantation finder functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,104 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { getDistance } from 'geolib';
 import './App.css'
 
+import customLocations from './assets/customerLocations.json'
+import plantationProjects from './assets/plantationProjects.json'
+import {GeolibInputCoordinates} from "geolib/es/types";
+import PlantationCard from "./components/PlantationCard";
+
+interface Plantation {
+  id: number,
+  type: string,
+  projectName: string,
+  status: string,
+  forestOwnership: string,
+  forestOwner: string,
+  treeQuantity: number,
+  location: string,
+  coordinatesUrl: string,
+  latitude: string,
+  longitude: string,
+  startId: number,
+  endId: number,
+  startDate: string,
+  comment: string,
+  area: string
+}
+
+interface Location {
+  name: string,
+  latitude: number,
+  longitude: number
+}
+
+
+function sortPlantationsByDistanceToLocation(plantations: Plantation[], location: Location): Plantation[] {
+  return plantations.sort((a, b) => {
+    const aCoords = { latitude: Number(a.latitude), longitude: Number(a.longitude) };
+    const bCoords = { latitude: Number(b.latitude), longitude: Number(b.longitude) };
+
+    const distanceA = getDistance(location, aCoords);
+    const distanceB = getDistance(location, bCoords);
+    return distanceA - distanceB;
+  })
+}
+
+function getDistancesOfNearestThree(plantations: Plantation[], location: Location): { plantation: Plantation, distance: number }[] {
+  var distances: { plantation: Plantation, distance: number }[] = []
+  for (let i = 0; i < 3; i++ ) {
+    let distance = getDistance(location, { latitude: Number(plantations[i].latitude), longitude: Number(plantations[i].longitude) })
+    distances.push({ plantation: plantations[i], distance: distance })
+  }
+  return distances
+}
+
+function getNearestPlantations(plantations: Plantation[], location: Location): { plantation: Plantation, distance: number }[] {
+
+  var sortedPlantations = sortPlantationsByDistanceToLocation(plantations, location)
+
+  return getDistancesOfNearestThree(sortedPlantations, location)
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const plantations:Plantation[] = plantationProjects as Plantation[]
+  const locations:Location[] = customLocations as Location[]
+  const [currentLocation, setCurrentLocation] = useState(locations[0].name)
+  const [currentPlantations, setCurrentPlantations] = useState(getNearestPlantations(plantations, locations[0]))
+
+
+
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    let searchedLocations = locations.filter(e => e.name === event.target.value)
+    if (searchedLocations.length > 0) {
+      setCurrentPlantations(getNearestPlantations(plantations, searchedLocations[0]))
+      setCurrentLocation(searchedLocations[0].name)
+    }
+
+  }
+
+
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <h1>{currentLocation}</h1>
+      {currentPlantations.map((plantations_distance) => (
+              <div className="centered" key={plantations_distance.plantation.id} >
+                <PlantationCard projectName={plantations_distance.plantation.projectName} location={plantations_distance.plantation.location} distance={plantations_distance.distance} />
+              </div>
+              ))}
     </div>
   )
 }
---
+++ src/components/PlantationCard.tsx
@@ -0,0 +1,19 @@
+import {CCard, CCardBody, CCardTitle} from "@coreui/react";
+
+type PlantationCardProps = {
+    projectName: string,
+    location: string,
+    distance: number
+}
+
+export const PlantationCard = ({ projectName, location, distance }: PlantationCardProps ) => {
+    return (
+            <div className="card">
+                <h2>{projectName}</h2>
+                <p>{location}</p>
+                <p>{distance % 1000.0}km</p>
+            </div>
+    )
+}
+
+export default PlantationCard
